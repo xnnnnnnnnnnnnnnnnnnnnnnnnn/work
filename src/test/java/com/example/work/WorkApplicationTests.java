0f793af316085ea9284313d72a663935de311520
xn <2120766940@qq.com>
2023-09-07 21:05:16 +0800

    Initial commit

---
+++ src/test/java/com/example/work/WorkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootApplication
+class WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
