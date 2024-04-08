bbcd520eeeb1daa862cc4b0baf88b9e54154dda9
xn <2120766940@qq.com>
2024-04-09 00:42:58 +0800

    Changes

--- src/main/java/com/example/work/WorkApplication.java
+++ src/main/java/com/example/work/WorkApplication.java
@@ -2,12 +2,19 @@ package com.example.work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+
 public class WorkApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(WorkApplication.class, args);
     }
+
+
 
 }
